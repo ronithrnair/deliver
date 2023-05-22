f34a8bca917cbd6e467695ce209083b75ae44189
ronithrnair <ronithrnair@gmail.com>
2023-05-22 12:01:08 +0530

    Commit 1

---
+++ customer/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Restaurant(models.Model):
+    restaurant_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='restaurant_images/')
+    
+    def __str__(self):
+        return self.restaurant_name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('Category', related_name='item')
+    restaurant = models.ForeignKey('Restaurant',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField(
+        'MenuItem', related_name='order', blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    roll_no = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    is_shipped = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
---
+++ customer/views.py
@@ -0,0 +1,161 @@
+import json
+from django.shortcuts import render, redirect
+from django.views import View
+from django.db.models import Q
+from django.core.mail import send_mail
+from .models import MenuItem, Category, OrderModel,Restaurant
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        restaurants = Restaurant.objects.all()
+        context = {
+            'restaurants' : restaurants
+        }
+        return render(request, 'customer/index.html',context)
+
+    def post(self, request, *args, **kwargs):
+
+        items = request.POST.get('r_options')
+        # rpk = Restaurant.objects.get(pk = items)
+        return redirect('order', pk = items)
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')
+
+
+class Order(View):
+    def get(self, request, pk = None,*args, **kwargs):
+        # get every item from each category
+        starters = MenuItem.objects.filter(restaurant__pk = pk).filter(category__name__contains='Starter')
+        mains = MenuItem.objects.filter(restaurant__pk = pk).filter(category__name__contains='Main')
+        desserts = MenuItem.objects.filter(restaurant__pk = pk).filter(category__name__contains='Dessert')
+        beverages = MenuItem.objects.filter(restaurant__pk = pk).filter(category__name__contains='Beverages')
+
+        # pass into context
+        context = {
+            'id' : pk,
+            'starters': starters,
+            'mains': mains,
+            'desserts': desserts,
+            'beverages': beverages,
+        }
+
+        # render the template
+        return render(request, 'customer/order.html',context)
+
+    def post(self, request, *args, **kwargs):
+        id = request.POST.get('id')
+        restaurant = Restaurant.objects.get(pk = id)
+        name = request.POST.get('name')
+        roll_no = request.POST.get('roll_no')
+        street = request.POST.get('street')
+        # data = get_context_data(**kwargs)
+        print(args)
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            roll_no = roll_no,
+            street=street,
+            restaurant= restaurant
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        # send_mail(
+        #     'Thank You For Your Order!',
+        #     body,
+        #     'example@example.com',
+        #     [email],
+        #     fail_silently=False
+        # )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        data = json.loads(request.body)
+
+        if data['isPaid']:
+            order = OrderModel.objects.get(pk=pk)
+            order.is_paid = True
+            order.save()
+
+        return redirect('payment-confirmation')
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
+
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        menu_items = MenuItem.objects.all()
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
